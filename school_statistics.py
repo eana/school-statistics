5579ffbaff0e9a6f6c4cc30339b8c8695c47f8f4
Jonas Eana <jonas@eana.ro>
2021-03-16 22:24:25 +0100

    WIP

--- school_statistics.py
+++ school_statistics.py
@@ -4,85 +4,104 @@ import os
 import re
 from pathlib import Path
 
+import click
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-headers = [
-    "School",
-    "Ämne",
-    "Total",
-    "Flickor",
-    "Pojkar",
-    "Total",
-    "Flickor",
-    "Pojkar",
-    "Total",
-    "Flickor",
-    "Pojkar",
-]
 
-kommun = {
-    'stockholm': '0180',
-    'huddinge': '0126'
-}
+@click.command()
+@click.option(
+    "--grade",
+    required=True,
+    type=int,
+    help="The grade for which you want me to get the statistics",
+)
+def main(grade):
+    headers = [
+        "School",
+        "Ämne",
+        "Totalt",
+        "Flickor",
+        "Pojkar",
+        "Betygspoäng - Totallt",
+        "Andel (%) med A-E1 - Totallt",
+        "Betygspoäng - Flickor",
+        "Andel (%) med A-E2 - Flickor",
+        "Betygspoäng3 - Pojkar",
+        "Andel (%) med A-E3 - Pojkar",
+    ]
 
+    kommun = {
+        "stockholm": "0180",
+        "huddinge": "0126"
+    }
 
-def get_all_schools(path, kommun_namn, kommun_cod):
-    Path(path).mkdir(parents=True, exist_ok=True)
+    def get_all_schools(path, kommun_namn, kommun_cod, grade):
+        Path(f"{path}-{grade}").mkdir(parents=True, exist_ok=True)
 
-    df = pd.read_csv(f"./{path}.csv")
+        df = pd.read_csv(f"{path}.csv")
 
-    for school in df.iloc:
-        value, name = school
-        url = f"https://siris.skolverket.se/reports/rwservlet?cmdkey=common&geo=1&report=gr6_betyg_amne&p_flik=G&p_verksamhetsar=2019&p_hmantyp=01&p_hmankod=&p_lankod=01&p_kommunkod={kommun_cod}&p_skolkod={value}"
-        r = requests.get(url)
+        for school in df.iloc:
+            value, name = school
 
-        pattern = 'rep_out.*.xls'
-        urlpart = re.search(pattern, r.content.decode('ISO-8859-1')).group()
-        url = f'https://siris.skolverket.se/{urlpart}'
-        r = requests.get(url)
-        open(f'./{path}/{name}.xls', 'wb').write(r.content)
+            if grade == 6:
+                # 6th grade
+                url = f"https://siris.skolverket.se/reports/rwservlet?cmdkey=common&geo=1&report=gr6_betyg_amne&p_flik=G&p_verksamhetsar=2019&p_hmantyp=01&p_hmankod=&p_lankod=01&p_kommunkod={kommun_cod}&p_skolkod={value}"
+            else:
+                # 9th grade
+                url = f"https://siris.skolverket.se/reports/rwservlet?cmdkey=common&geo=1&report=gr_betyg_amne&p_flik=G&p_ar=2019&p_lankod=01&p_kommunkod={kommun_cod}&p_skolkod=&p_hmantyp=&p_hmankod=&p_flik=G"
 
+            r = requests.get(url)
+            pattern = "rep_out.*.xls"
+            urlpart = re.search(
+                pattern, r.content.decode("ISO-8859-1")).group()
+            url = f"https://siris.skolverket.se/{urlpart}"
+            r = requests.get(url)
+            open(f"./{path}-{grade}/{name}.xls", "wb").write(r.content)
 
-def read_html(filename):
-    with open(filename, "r", encoding="ISO-8859-1") as f:
-        soup = BeautifulSoup(f, features="html.parser")
-        table = soup.find_all('table')[0]
-        rows = table.find_all('tr')
-        sheetname = os.path.splitext(os.path.basename(filename))[0]
-        headers = ['School'] + [x.string for x in rows[9].find_all('td')]
-        if len(rows[10].find_all('td')) == 1:
-            return
+    def read_html(path):
+        paths = glob.glob(f"{path}-{grade}/*.xls")
+        paths.sort()
 
-        rows = rows[10:33]
+        dfObj = pd.DataFrame(columns=headers)
+        writer = pd.ExcelWriter(f"{path}.xlsx", engine="xlsxwriter")
 
-        global dfObj
-        for row in rows:
-            values = [sheetname] + [x.string for x in row.find_all('td')]
-            dfObj = dfObj.append(dict(zip(headers, values)), ignore_index=True)
-        global writer
+        for filename in paths:
+            with open(filename, "r", encoding="ISO-8859-1") as f:
+                soup = BeautifulSoup(f, features="html.parser")
+                table = soup.find_all("table")[0]
+                rows = table.find_all("tr")
+                sheetname = os.path.splitext(os.path.basename(filename))[0]
+
+                if len(rows[11].find_all("td")) == 1:
+                    continue
+
+                rows = rows[11:30]
+
+                for row in rows:
+                    values = [sheetname] + \
+                        [x.string for x in row.find_all("td")]
+                    dfObj = dfObj.append(
+                        dict(zip(headers, values)), ignore_index=True)
+
         dfObj.to_excel(writer, sheet_name="Sheet", index=False)
+        writer.save()
 
+    for key in kommun:
+        # the grade can be 6 or 9
+        grade = 9
+        path = f"./schools-{key}"
+        kommun_namn = key
+        kommun_cod = kommun[key]
 
-for key in kommun:
-    path = f"./schools-{key}"
-    kommun_namn = key
-    kommun_cod = kommun[key]
+        print(f"Kommun: {kommun_namn}")
+        print("Get all schools!")
+        get_all_schools(path, kommun_namn, kommun_cod, grade)
 
-    dfObj = pd.DataFrame(columns=headers)
-    writer = pd.ExcelWriter(f"{path}.xlsx", engine="xlsxwriter")
-
-    print(f"Kommun: {kommun_namn}")
-    print("Get all schools!")
-    get_all_schools(path, kommun_namn, kommun_cod)
-
-    print("Create the excel file!\n")
-    paths = glob.glob(f"{path}/*.xls")
-    paths.sort()
-
-    for path in paths:
-        # print(path)
+        print("Create the excel file!\n")
         read_html(path)
 
-    writer.save()
+
+if __name__ == "__main__":
+    main()
